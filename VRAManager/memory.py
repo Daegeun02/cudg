0179a9ebf3379db9f89e4669d680e03091305871
Daegeun02 <redhawkdg02@gmail.com>
2023-05-21 23:58:50 +0900

    prototype of VRAM manager

--- VRAManager/memory.py
+++ VRAManager/memory.py
@@ -1,6 +1,27 @@
+import pycuda.driver as cuda
 
 
 
 class Memory:
 
-    pass
+    type = cuda.DeviceAllocation
+
+
+    def _request( self, size: int ) -> cuda.DeviceAllocation:
+        '''
+        for each number has 4 byte 
+        because GPU uses 32bit
+        '''
+        return cuda.mem_alloc( size * 4 )
+
+
+    def _return( self, mem: cuda.DeviceAllocation ) -> None:
+
+        try:
+            mem.free()
+        except:
+            print( 'Memory Link might be happened' )
+            print( 'Recommand to restart kernel' )
+
+
+VRAM = Memory()
