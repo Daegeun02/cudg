72a6fbf46cde5a1f493cdc5e885956c463e2fb1a
daegeun <redhawkdg02@gmail.com>
2025-03-09 20:35:08 +0900

    heyhey

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='CUDG',
+    name='cudg',
     version='0.0.2',
     description='CUDG is for gpuarray type for convenience',
     author='Daegeun02',
--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./build
 rm -rf ./dist
-rm -rf ./Pygeun.egg-info
+rm -rf ./cudg.egg-info
 
 python3 setup.py sdist bdist_wheel
 
